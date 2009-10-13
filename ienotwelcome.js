486cb50d90662e17b7bf56cbba08d93d645aa970
Nicholas Firth-McCoy <nfm@deagle.(none)>
2009-10-14 10:47:01 +1100

    Began fixing browser detection

--- ienotwelcome.js
+++ ienotwelcome.js
@@ -24,14 +24,41 @@ function getARealBrowser() {
 
 // Check whether the user's browser sucks
 function browserSucks() {
-	<!--[if lte IE 6] >
-		return true;
-	<![endif] -->
+	// Should IE be allowed at all?
+	var allowIE = true;
+
+	// If allowIE is true, what minimum version of IE doesn't suck?
+	var minimumIEVersion = 8;
+
+	var appVer = navigator.appVersion.toLowerCase();
+	var is_ie, is_minor, is_major;
+	var is_ie6, is_ie6up, is_ie7, is_ie7up;
+
+	if (appVer.indexOf('msie') != -1) {
+		is_ie = true;	
+	};
+
+	// If the user's browser is IE
+	if (is_ie) {
+
+		// If allowIE is false, reject their browser
+		if (allowIE == false) {
+			return true;
+		}
+
+		// Determine IE's version numbers
+		is_minor = parseFloat(appVer.substring(is_ie + 5, appVer.indexOf(';', is_ie)));
+		is_major = parseInt(is_minor);
+
+		if (is_major < minimumIEVersion) {
+			return true;
+		}
+	}
 
 	return false;
 }
 
 // The fun starts here
-if browserSucks() {
+if (browserSucks()) {
 	getARealBrowser();
 }
